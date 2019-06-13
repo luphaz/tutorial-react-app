aaad7246ece551db5ff2918ba8ae68df848fd979
luphaz <matthieu.bono@icloud.com>
2019-06-13 15:31:44 +0200

    fix: remove redux

--- src/index.js
+++ src/index.js
@@ -3,14 +3,6 @@ import ReactDOM from "react-dom";
 import Game from "./components/Game";
 import "./index.css";
 
-import { Provider } from "react-redux";
-import store from "./redux/store";
-
 // ========================================
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Game />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Game />, document.getElementById("root"));
